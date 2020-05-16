3df4ffea0f90aa2d59e24efe17c14cbbcc67d00e
MartinWang <martinwang7963@gmail.com>
2020-05-16 12:47:12 +0800

    Add Repository Pattern

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,16 +1,39 @@
+using System.Threading.Tasks;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers {
     [ApiController]
     [Route ("api/[controller]")]
     public class ProductController : ControllerBase {
-        public ProductController()
-        {
+        private IProductRepository _repo;
+        public ProductController (IProductRepository repo) {
+            _repo = repo;
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<ActionResult> GetProductByIdAsync (int id) {
+            var products = await _repo.GetProductByIdAsync (id);
+            return Ok (products);
         }
 
         [HttpGet]
-        public string GetProducts () {
-            return "products";
+        public async Task<ActionResult> GetProductsAsync () {
+            var products = await _repo.GetProductsAsync ();
+            return Ok (products);
         }
+
+        [HttpGet ("brands")]
+        public async Task<ActionResult> GetProductBrandsAsync () {
+            var productBrands = await _repo.GetProductBrandsAsync ();
+            return Ok (productBrands);
+        }
+
+        [HttpGet ("brands")]
+        public async Task<ActionResult> GetProductTypesAsync () {
+            var productTypes = await _repo.GetProductTypesAsync ();
+            return Ok (productTypes);
+        }
+
     }
 }
